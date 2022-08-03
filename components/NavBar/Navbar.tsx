ff2f07809856dd20b33fc1f1b54e7dcdc9c21d72
Tccc-web <visaltycoon@gmail.com>
2022-08-03 15:54:01 +1000

    navbar

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,24 +1,30 @@
-import styles from './Navbar.module.scss'
-import { Flex, Box, Spacer } from '@chakra-ui/react';
+import styles from "./NavBar.module.scss";
+import { Flex, Box, Spacer } from "@chakra-ui/react";
 
-export default function Navbar() {
-    return (
-        <div className={styles.navcontainer}>
-            <div className={styles.nav}>
-                <Flex minWidth='max-content' alignItems='center' justifyContent='start'>
-                    <Box p='4' className={styles.left}>
-                        <a href='#'>Create Project</a>
-                    </Box>
-                    <Spacer />
-                    <Box className={styles.center} justifyContent='center' >
-                        <a href='#'>MICRORAPTOR</a>
-                    </Box>
-                    <Spacer />
-                    <Box className={styles.right} justifyContent='end'>
-                        <a href='#'>Sign in / Sign up</a>
-                    </Box>
-                </Flex>
-            </div>
-        </div >
-    );
+export default function NavBar() {
+  return (
+    <div className={styles.navcontainer}>
+      <div className={styles.nav}>
+        <Flex minWidth="max-content" alignItems="center" justifyContent="start">
+          <Box p="4" className={styles.left}>
+            <a href="#">Create Project</a>
+          </Box>
+          <Box p="4" className={styles.leftsmall}>
+            <a href="#">Create</a>
+          </Box>
+          <Spacer />
+          <Box className={styles.center} justifyContent="center">
+            <a href="#">MICRORAPTOR</a>
+          </Box>
+          <Spacer />
+          <Box className={styles.right} justifyContent="end">
+            <a href="#">Sign in / Sign up</a>
+          </Box>
+          <Box className={styles.rightsmall} justifyContent="end">
+            <a href="#">Sign in</a>
+          </Box>
+        </Flex>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/NavBar/Navbar'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import NavBar from "../components/NavBar/NavBar";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
+      <NavBar />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -62,15 +62,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
