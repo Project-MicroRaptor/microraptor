dc9dae3c0db52e56b2bf4d3803dba05c920c681d
Brandon <BrandonKoala@users.noreply.github.com>
2022-08-04 14:19:53 +1000

    Add next-auth and Auth0 (#5)
    Approved by Tycoon and Rohan. Merged using 'Squash and Merge.' 4/8/22

---
+++ theme/components/Button.tsx
@@ -0,0 +1,16 @@
+const ButtonStyles = {
+  variants: {
+    base: {
+      bg: "brand.primary",
+      color: "white",
+      _hover: {
+        bg: "brand.hover",
+      },
+    },
+  },
+  defaultProps: {
+    variant: "base",
+  },
+};
+
+export default ButtonStyles;
