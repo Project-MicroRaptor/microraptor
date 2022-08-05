6943a02c85a4e5516f9d113c2e81e385104ae26e
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-08-05 20:02:11 +1000

    navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,30 +1,35 @@
 import styles from "./NavBar.module.scss";
 import { Flex, Box, Spacer } from "@chakra-ui/react";
+import useWindowResize from './../../pages/hooks/useWindowResize';
 
 export default function NavBar() {
-  return (
-    <div className={styles.navcontainer}>
-      <div className={styles.nav}>
-        <Flex minWidth="max-content" alignItems="center" justifyContent="start">
-          <Box p="4" className={styles.left}>
-            <a href="#">Create Project</a>
-          </Box>
-          <Box p="4" className={styles.leftsmall}>
-            <a href="#">Create</a>
-          </Box>
-          <Spacer />
-          <Box className={styles.center} justifyContent="center">
-            <a href="#">MICRORAPTOR</a>
-          </Box>
-          <Spacer />
-          <Box className={styles.right} justifyContent="end">
-            <a href="#">Sign in / Sign up</a>
-          </Box>
-          <Box className={styles.rightsmall} justifyContent="end">
-            <a href="#">Sign in</a>
-          </Box>
-        </Flex>
-      </div>
-    </div>
-  );
+    const { width } = useWindowResize();
+
+    let createText = "Create Project";
+    let signInText = "Sign in / Sign up";
+
+    if (width && width < 673) {
+        createText = "Create";
+        signInText = "Sign in";
+    }
+
+    return (
+        <div className={styles.navcontainer}>
+            <div className={styles.nav}>
+                <Flex minWidth="max-content" alignItems="center" justifyContent="start">
+                    <Box p="4" className={styles.left}>
+                        <a href="#">{createText}</a>
+                    </Box>
+                    <Spacer />
+                    <Box className={styles.center} justifyContent="center">
+                        <a href="#">MICRORAPTOR</a>
+                    </Box>
+                    <Spacer />
+                    <Box className={styles.right} justifyContent="end">
+                        <a href="#">{signInText}</a>
+                    </Box>
+                </Flex>
+            </div>
+        </div>
+    );
 }
