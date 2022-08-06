48762f9c33b7012695662fabb12f253bf9b871a4
Jye J. Stillitano <jyestillitano@hotmail.com>
2022-08-06 17:56:17 +1000

    Jye/MC-83 Create project database schema (#7)
    * Add Prisma integration with next-auth
    
    * Project and ProjectReward tables added to the database schema.
    
    * Altered fields and types. Removed Category enum.
    
    * Added project completedAt field.
    
    * Migration recreated.
    
    * Altered dbgenerated default to prevent continuous schema change bug.
    
    Co-authored-by: BrandonKoala <BrandonKoala@users.noreply.github.com>

---
+++ prisma/migrations/20220806040821_project_tables/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "completedAt" TIMESTAMP(3) NOT NULL DEFAULT (now() + '1 year'::interval),
+    "ownerId" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "targetFunding" DOUBLE PRECISION NOT NULL,
+    "currentFunding" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "postcode" INTEGER NOT NULL,
+    "shortDescription" TEXT NOT NULL,
+    "aboutBusiness" TEXT,
+    "aboutOwner" TEXT,
+    "businessPlan" TEXT,
+    "categories" TEXT[],
+    "images" TEXT[],
+    "attachments" TEXT[],
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectReward" (
+    "id" TEXT NOT NULL,
+    "projectID" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "cost" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "ProjectReward_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectReward" ADD CONSTRAINT "ProjectReward_projectID_fkey" FOREIGN KEY ("projectID") REFERENCES "Project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
