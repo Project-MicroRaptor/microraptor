34a836f5954ce69105d73302a928797a807b8f9b
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-08-06 17:33:23 +1000

    change -> crossOrigin="true"

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap"
             rel="stylesheet"
