e85458c0313a6f46300b7a611d190ba6c2d7b007
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-08-09 20:15:02 +1000

    Added inbox and profile icons after login

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,8 +1,12 @@
 import styles from "./NavBar.module.scss";
 import { Flex, Box, Spacer } from "@chakra-ui/react";
 import useWindowResize from './../../hooks/useWindowResize';
+import { useSession } from "next-auth/react";
+import { BsFillInboxFill } from "react-icons/bs";
+import { Avatar } from '@chakra-ui/react'
 
 export default function NavBar() {
+    const { data: session } = useSession();
     const { width } = useWindowResize();
 
     let createText = "Create Project";
@@ -11,6 +15,31 @@ export default function NavBar() {
     if (width && width < 673) {
         createText = "Create";
         signInText = "Sign in";
+    }
+
+    if (session) {
+        return (
+            <div className={styles.navcontainer}>
+                <div className={styles.nav}>
+                    <Flex minWidth="max-content" alignItems="center" justifyContent="start">
+                        <Box p="4" className={styles.side}>
+                            <a href="#">{createText}</a>
+                        </Box>
+                        <Spacer />
+                        <Box className={styles.center} justifyContent="center">
+                            <a href="#">MICRORAPTOR</a>
+                        </Box>
+                        <Spacer />
+                        <Box className={styles.inbox} justifyContent="end">
+                            <a href="#"><BsFillInboxFill fontSize="30px" /></a>
+                        </Box>
+                        <Box className={styles.profile} justifyContent="end">
+                            <a href="#"><Avatar src='https://bit.ly/broken-link' size="sm" /></a>
+                        </Box>
+                    </Flex>
+                </div>
+            </div>
+        );
     }
 
     return (
