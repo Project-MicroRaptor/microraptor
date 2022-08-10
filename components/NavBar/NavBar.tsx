08fa143db6c02d2a37d477b0d267203d5b455de5
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-08-10 14:27:09 +1000

    Implement profile picture dropdown

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
 import styles from "./NavBar.module.scss";
-import { Flex, Box, Spacer } from "@chakra-ui/react";
+import { Flex, Box, Spacer, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import useWindowResize from "./../../hooks/useWindowResize";
 import Link from "next/link";
 import { useSession, signOut, signIn } from "next-auth/react";
 import { BsFillInboxFill } from "react-icons/bs";
+import { AiFillSetting, AiOutlineLogout, AiOutlineProject } from "react-icons/ai";
 import { Avatar } from '@chakra-ui/react'
 
 export default function NavBar() {
@@ -39,13 +40,42 @@ export default function NavBar() {
               <Box className={styles.inbox} justifyContent="end">
                 <a><BsFillInboxFill fontSize="30px" /></a>
               </Box>
-              <Box className={styles.profile} justifyContent="end">
-                <a onClick={() =>
-                  signOut({
-                    callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000"}/api/auth/logout`,
-                  })
-                }><Avatar src={session.user?.image ?? ""} size="sm" /></a>
-              </Box>
+              <Menu>
+                <MenuButton className={styles.menu}>
+                  <Box className={styles.profile} justifyContent="end">
+                    <a>
+                      <Avatar src={session.user?.image ?? ""} size="sm" />
+                    </a>
+                  </Box>
+                </MenuButton>
+                <MenuList className={styles.menuList}>
+                  <a>
+                    <Avatar className={styles.sessionImage} src={session.user?.image ?? ""} size="md" />
+                    <span className={styles.sessionName}>{session.user?.name}</span>
+                    <span className={styles.viewProfile}>
+                      <a href="#">View Profile</a>
+                    </span>
+                  </a>
+                  <div className={styles.divider}>
+                    <div className={styles.settingDropdown}>
+                      <span className={styles.settingIcon}><AiFillSetting /></span>
+                      <a href="#" className={styles.settings}>Settings</a>
+                    </div>
+                    <div className={styles.projectDropdown}>
+                      <span className={styles.projectIcon}><AiOutlineProject /></span>
+                      <a href="#" className={styles.project}>Your Project</a>
+                    </div>
+                    <div className={styles.signoutDropdown}>
+                      <span className={styles.projectIcon}><AiOutlineLogout /></span>
+                      <a href="#" onClick={() =>
+                        signOut({
+                          callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000"}/api/auth/logout`,
+                        })
+                      } className={styles.signout}>Sign out</a>
+                    </div>
+                  </div>
+                </MenuList>
+              </Menu>
             </>
           ) : (
             <Box className={styles.side} justifyContent="end">
