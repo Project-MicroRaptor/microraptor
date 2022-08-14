f201cdc6f234bc4ae91c451410cfe68a702eb38c
Jye J. Stillitano <jyestillitano@hotmail.com>
2022-08-14 12:36:52 +1000

    No image on ProjectCard process optimised.
    Semicolons and linebreaks added where missing.

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -10,26 +10,18 @@ export interface ProjectProps {
 }
 
 export default function ProjectCard(props: ProjectProps) {
-  // No image provided, default image. 
-  // Stretch goal, check if URL returns an image. 
-  let imageElement
-  let imageSrc = props.image
-  if (imageSrc == null) {
-    imageElement = <></>
-  } else {
-    imageElement = <img
-      src={imageSrc}
-      alt={props.name}
-    />
-  }
-
   return (
     // TODO: href direct to project page.
     <a className={styles.projectContainer}>
       <div className={styles.imageContainer}>
-        {imageElement}
+        {props?.image && (
+          <img
+            src={props?.image}
+            alt={props.name}
+          />
+        )}
       </div>
-      
+
       <div className={styles.infoContainer}>
         <h2>{props.name}</h2>
         <p>{props.shortDescription}</p>
@@ -37,12 +29,12 @@ export default function ProjectCard(props: ProjectProps) {
 
       <div className={styles.progressContainer}>
         <Progress
-            value={(props.currentFunding / props.targetFunding) * 100}
-            size="sm"
-            borderStartRadius={30}
-            borderEndRadius={30}
-          />
-          <p>${props.currentFunding} of ${props.targetFunding} already raised!</p>
+          value={(props.currentFunding / props.targetFunding) * 100}
+          size="sm"
+          borderStartRadius={30}
+          borderEndRadius={30}
+        />
+        <p>${props.currentFunding.toLocaleString()} of ${props.targetFunding.toLocaleString()} already raised!</p>
       </div>
     </a>
   );
