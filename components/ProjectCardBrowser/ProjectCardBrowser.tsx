62ca14a861563056baec50c7a2c4244e5bc85d23
Jye J. Stillitano <jyestillitano@hotmail.com>
2022-08-19 10:00:59 +1000

    Request for getServerSideProps on index changed to an API route within the ProjectBrowser component.
    ProjectBrowser styling approach altered.

--- components/ProjectCardBrowser/ProjectCardBrowser.tsx
+++ components/ProjectCardBrowser/ProjectCardBrowser.tsx
@@ -1,51 +1,66 @@
-import styles from "./ProjectCardBrowser.module.scss"
+import styles from "./ProjectCardBrowser.module.scss";
 import ProjectCard from "../ProjectCard/ProjectCard";
-import { SimpleGrid, Spinner, Grid } from '@chakra-ui/react'
-import useSWR from 'swr'
+import { SimpleGrid, Spinner, Grid } from "@chakra-ui/react";
+import useSWR from "swr";
+import { fetcher } from "../../utils/swr";
 
 type Projects = {
-    id: string
-    name: string
-    shortDescription: string
-    images: string[]
-    currentFunding: number
-    targetFunding: number
+  id: string;
+  name: string;
+  shortDescription: string;
+  images: string[];
+  currentFunding: number;
+  targetFunding: number;
 }[];
 
-const fetcher = async () => {
-    const response = await fetch('http://localhost:3000/api/projects');
-    const data: Projects = await response.json();
-    return data;
-}
-
 export default function ProjectCardBrowser() {
-    const { data, error } = useSWR('projects', fetcher);
+  const { data, error } = useSWR<Projects>("/api/projects", fetcher);
 
-    // Error message.
-    if (error) return <div>An error occured.</div>;
+  // Error message.
+  if (error) return <div>An error occured.</div>;
 
-    // Waiting for data.
-    if (!data) return <Spinner />;
-
-    // No projects.
-    if (data.length == 0) {
-        return (<p className={styles.noProjects}>There are currently no projects looking for funding. Why not start one by selecting Create Project in the upper left hand corner?</p>);
-    }
-
+  // Waiting for data.
+  if (!data)
     return (
-        <SimpleGrid className={styles.grid} columns={[1,2,3,4]} minChildWidth="350px" spacing="20px">
-            {data.map(project => {
-                return (
-                    <ProjectCard
-                        key={project.id}
-                        name={project.name}
-                        shortDescription={project.shortDescription}
-                        image={project.images[0]}
-                        currentFunding={project.currentFunding}
-                        targetFunding={project.targetFunding}
-                    />
-                );
-            })}
-        </SimpleGrid>
+      <div className={styles.spinnerContainer}>
+        <Spinner
+          margin="auto"
+          width="200px"
+          height="200px"
+          thickness="12px"
+          color="brand.primary"
+          emptyColor="gray.200"
+          speed="1s"
+        />
+      </div>
     );
+
+  // No projects.
+  if (data.length == 0) {
+    return (
+      <p className={styles.noProjects}>
+        There are currently no projects looking for funding. Why not start one
+        by selecting Create Project in the upper left hand corner?
+      </p>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+        <SimpleGrid className={styles.grid} spacing="40px">
+          {data.map((project) => {
+            return (
+              <ProjectCard
+                key={project.id}
+                name={project.name}
+                shortDescription={project.shortDescription}
+                image={project.images[0]}
+                currentFunding={project.currentFunding}
+                targetFunding={project.targetFunding}
+              />
+            );
+          })}
+        </SimpleGrid>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ const Home: NextPage = () => {
       <NavBar />
 
       <ProjectCardBrowser />
+      
     </div>
   );
 };
