22b4ea191924dd1b1d4955df3466036b35ff70f1
Brandon <BrandonKoala@users.noreply.github.com>
2022-08-22 19:36:02 +1000

    Add spinner when checking if logged in to AuthGuard (#15)

--- components/AuthGuard/AuthGuard.tsx
+++ components/AuthGuard/AuthGuard.tsx
@@ -1,18 +1,42 @@
+import { Spinner } from "@chakra-ui/react";
 import { getSession, signIn } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import styles from "./AuthGuard.module.scss";
 
 export function AuthGuard({ children }: { children: JSX.Element }) {
+  const [hasSession, setHasSession] = useState(false);
+
   useEffect(() => {
     const fetchSession = async () => {
       const session = await getSession();
 
       if (!session) {
         signIn("auth0");
+        return false;
       }
-    }
 
-    fetchSession();
-  });
+      return true;
+    };
+
+    fetchSession().then((session) => setHasSession(session));
+  }, [setHasSession]);
+
+  if (!hasSession) {
+    return (
+      <div className={styles.spinner}>
+        <Spinner
+          margin="auto"
+          width="200px"
+          height="200px"
+          thickness="12px"
+          color="brand.primary"
+          emptyColor="gray.200"
+          speed="1s"
+        />
+      </div>
+    );
+  }
 
   return children;
 }
