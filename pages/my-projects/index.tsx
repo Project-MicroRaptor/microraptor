72a27c56ad6b2415c1823ce80cfeb6c37d468605
Isaac <carr.isaac09@gmail.com>
2022-08-30 20:56:05 +1000

    wip

--- pages/my-projects/index.tsx
+++ pages/my-projects/index.tsx
@@ -9,16 +9,29 @@ import type { ProjectCards } from "../../types/projectTypes";
 
 import styles from "./myProjects.module.scss";
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
+import {
+  LeftArrow,
+  RightArrow,
+} from "../../components/HorizontalSlider/arrows";
 
 const MyProjects: AuthNextPage = (props) => {
   const { data, error } = useSWR<ProjectCards>("/api/my-projects", fetcher);
 
-  const Arrow = ({ text, className }: { text: string; className: string }) => {
-    return <div className={className}>{text}</div>;
-  };
+  const Arrows = () => (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        fontSize: "2.5em",
+      }}
+    >
+      <div style={{ marginLeft: "10px", display: "flex" }}>
+        <LeftArrow /> <RightArrow />
+      </div>
+    </div>
+  );
 
-  const ArrowLeft = Arrow({ text: "<", className: "arrow-prev" });
-  const ArrowRight = Arrow({ text: ">", className: "arrow-next" });
   const cards = data?.map((project) => {
     return (
       <div className={styles.card}>
@@ -44,11 +57,7 @@ const MyProjects: AuthNextPage = (props) => {
       <div className={styles.container}>
         <h1>Active Projects</h1>
         <div className={styles.slider}>
-          {cards && (
-            <ScrollMenu LeftArrow={ArrowLeft} RightArrow={ArrowRight}>
-              {cards}
-            </ScrollMenu>
-          )}
+          {cards && <ScrollMenu Header={Arrows}>{cards}</ScrollMenu>}
         </div>
       </div>
     </>
