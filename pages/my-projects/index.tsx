4db5178dfdae16ba6cd3c8c71389b0c7f42de54f
Isaac <carr.isaac09@gmail.com>
2022-08-30 12:15:46 +1000

    WIP

--- pages/my-projects/index.tsx
+++ pages/my-projects/index.tsx
@@ -7,13 +7,32 @@ import ProjectCardwButton from "../../components/ProjectCardwButton/ProjectCardw
 import type { AuthNextPage } from "../../types/appProps";
 import type { ProjectCards } from "../../types/projectTypes";
 
-
 import styles from "./myProjects.module.scss";
-
-
+import { ScrollMenu } from "react-horizontal-scrolling-menu";
 
 const MyProjects: AuthNextPage = (props) => {
   const { data, error } = useSWR<ProjectCards>("/api/my-projects", fetcher);
+
+  const Arrow = ({ text, className }: { text: string; className: string }) => {
+    return <div className={className}>{text}</div>;
+  };
+
+  const ArrowLeft = Arrow({ text: "<", className: "arrow-prev" });
+  const ArrowRight = Arrow({ text: ">", className: "arrow-next" });
+  const cards = data?.map((project) => {
+    return (
+      <div className={styles.card}>
+        <ProjectCardwButton
+          key={project.id} // Each child in a list should have a unique "key" prop.
+          name={project.name}
+          shortDescription={project.shortDescription}
+          image={project.images[0]}
+          currentFunding={project.currentFunding}
+          targetFunding={project.targetFunding}
+        />
+      </div>
+    );
+  });
   return (
     <>
       <Head>
@@ -21,20 +40,17 @@ const MyProjects: AuthNextPage = (props) => {
         <meta name="description" content="My Projects" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <NavBar />
-        <div className={styles.container}>
-          <h1>Active Projects</h1>
-        {data && data.map(project => {
-          return <ProjectCardwButton
-            key={project.id} // Each child in a list should have a unique "key" prop.
-            name={project.name}
-            shortDescription={project.shortDescription}
-            image={project.images[0]}
-            currentFunding={project.currentFunding}
-            targetFunding={project.targetFunding}
-          />
-        })}
+      <NavBar />
+      <div className={styles.container}>
+        <h1>Active Projects</h1>
+        <div className={styles.slider}>
+          {cards && (
+            <ScrollMenu LeftArrow={ArrowLeft} RightArrow={ArrowRight}>
+              {cards}
+            </ScrollMenu>
+          )}
         </div>
+      </div>
     </>
   );
 };
