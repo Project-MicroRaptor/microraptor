9c238fa8e870016c6ffd03f4ee57410fb3e3463c
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-09-02 07:17:29 +1000

    Tycoon/MC-126-Create top level project information in ProjectView (#18)
    * Added inbox and profile icons after login
    
    * Added hovering color for inbox icon
    
    * Removed LogInOutButton.tsx, refactoring & styling
    
    * removed LogInOutButton folder
    
    * Fix navbar height
    
    * Implement profile picture dropdown
    
    * removed a duplicate line (useSession)
    
    * refactor 'View Profile' layout
    
    * refactoring scss file,add folder for its component
    
    * refactor styling, put dropdown to new component
    
    * refactor class name, code formatting
    
    * refactor styling for dropdown
    
    * refactor menuList styling
    
    * refactor dropdown style
    
    * remove !important, implement Chakra UI's styling
    
    * refactor styling
    
    * refactor styling
    
    * remove !important from css
    
    * code formatted, remove unused import
    
    * Added information for project preview (demo)
    
    * Implemented Project Preview
    
    * Implemented Project Preview
    
    * fix build error, added export default...
    
    * code format (remove spacing)
    
    * move swr.tsx file outside the /page folder
    
    * refactor codes/css, added primary font
    
    * fix screen responsive, add default value
    
    * fix indentation
    
    * fix spacing
    
    * fix styling
    
    * fix icon scaling
    
    * add spinner, 404 page error, variable "backers"
    
    * add NavBar to [id].tsx file
    
    * added spinner on error page
    
    * add spinner to !data
    
    * clickable ProjectCard, fix icon resizing
    
    * missing key is resolved for ProjectCardBrowser
    
    * change media screen px, add in title to the tab
    
    * add id to ProjectCard
    
    * add Header/title
    
    * added space to categories.tsx, fix navbar styling
    
    * center button
    
    * add padding bottom to buttons
    
    * fix button styling

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -3,6 +3,7 @@ import { Progress } from "@chakra-ui/react";
 import styles from "./ProjectCard.module.scss";
 
 export interface ProjectProps {
+  id: string;
   name: string;
   shortDescription: string;
   image?: string;
@@ -12,8 +13,7 @@ export interface ProjectProps {
 
 export default function ProjectCard(props: ProjectProps) {
   return (
-    // TODO: href direct to project page.
-    <a className={styles.projectContainer}>
+    <a href={`project/${props.id}`} className={styles.projectContainer} >
       <div className={styles.imageContainer}>
         {props?.image && <img src={props?.image} alt={props.name} />}
       </div>
@@ -35,6 +35,6 @@ export default function ProjectCard(props: ProjectProps) {
           {props.targetFunding.toLocaleString()} already raised!
         </p>
       </div>
-    </a>
+    </a >
   );
 }
