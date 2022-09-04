0e9e3c84d84439c117a57ec545a535082a9f5c15
101511820S <101511820@student.swin.edu.au>
2022-09-04 11:02:54 +1000

    Clean up code

--- components/Settings/AccountSettings.tsx
+++ components/Settings/AccountSettings.tsx
@@ -1,13 +1,4 @@
 import styles from "./SettingsTabs.module.scss";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  Box,
-  HStack,
-  Center,
-} from "@chakra-ui/react";
 
 export default function AccountSettings() {
   return (
@@ -61,103 +52,6 @@ export default function AccountSettings() {
             </div>
           </form>
         </div>
-        {/* <FormControl>
-          <Box className={styles.accountbox}>
-            <HStack spacing="24px">
-              <FormLabel className={styles.formlabel}>Name</FormLabel>
-              <Input
-                className={styles.forminput}
-                placeholder="name"
-                size="md"
-                // borderColor="#ffffff"
-                textAlign="center"
-              />
-              <Button
-                className={styles.formbutton}
-                width="20%"
-                color="#17a2b8"
-                background="#ffffff"
-                variant="ghost"
-              >
-                Edit
-              </Button>
-            </HStack>
-          </Box>
-          <hr />
-          <Box className={styles.accountbox}>
-            <HStack spacing="24px">
-              <FormLabel className={styles.formlabel}>Email</FormLabel>
-              <Input
-                className={styles.forminput}
-                placeholder="email"
-                size="md"
-                borderColor="#ffffff"
-                textAlign="center"
-              />
-              <Button
-                className={styles.formbutton}
-                width="20%"
-                color="#17a2b8"
-                background="#ffffff"
-                variant="ghost"
-              >
-                Edit
-              </Button>
-            </HStack>
-          </Box>
-          <hr />
-          <Box className={styles.accountbox}>
-            <HStack spacing="24px">
-              <FormLabel className={styles.formlabel}>
-                Change Password
-              </FormLabel>
-              <Input
-                className={styles.forminput}
-                placeholder="password"
-                size="md"
-                borderColor="#ffffff"
-                textAlign="center"
-              />
-              <Button
-                className={styles.formbutton}
-                width="20%"
-                color="#17a2b8"
-                background="#ffffff"
-                variant="ghost"
-              >
-                Edit
-              </Button>
-            </HStack>
-          </Box>
-          <hr />
-          <Box className={styles.accountbox}>
-            <HStack spacing="24px">
-              <FormLabel className={styles.formlabel}>Address</FormLabel>
-              <Input
-                className={styles.forminput}
-                placeholder="address"
-                size="md"
-                borderColor="#ffffff"
-                textAlign="center"
-              />
-              <Button
-                className={styles.formbutton}
-                width="20%"
-                color="#17a2b8"
-                background="#ffffff"
-                variant="ghost"
-              >
-                Edit
-              </Button>
-            </HStack>
-          </Box>
-          <hr className={styles.notificationdivider} />
-          <div className={styles.savebox}>
-            <Center>
-              <Button>Save Changes</Button>
-            </Center>
-          </div>
-        </FormControl> */}
       </div>
     </div>
   );
--- components/Settings/NotificationSettings.tsx
+++ components/Settings/NotificationSettings.tsx
@@ -1,30 +1,42 @@
 import styles from "./SettingsTabs.module.scss";
-import { Switch, Box, Flex, Spacer } from '@chakra-ui/react'
+import { Switch, Box, Flex, Spacer } from "@chakra-ui/react";
 
 export default function NotificationSettings() {
-    return (
-      <div className={styles.containertitle}>
-        <h1 className={styles.accounttitle}>Notification Settings</h1>
-        <hr className={styles.accountdivider}/>
+  return (
+    <div className={styles.containertitle}>
+      <h1 className={styles.accounttitle}>Notification Settings</h1>
+      <hr className={styles.accountdivider} />
+      <Box>
         <Box>
-          <Box className={styles.box} >
-            <Flex>
-              <Box><p className={styles.accountfields}>Project Updates</p></Box>
-              <Spacer />
-              <Box><Switch className={styles.switch} colorScheme='teal' size='md' /></Box>
-            </Flex>
-            <p className={styles.accountsubfields}>Receive emails about popular projects</p>
-          </Box>
-          <hr/>
-          <Box>
-            <Flex>
-                <Box><p className={styles.accountfields}>Message Enquires</p></Box>
-                <Spacer />
-                <Box><Switch className={styles.switch} colorScheme='teal' size='md' /></Box>
-            </Flex>
-            <p className={styles.accountsubfields}>Receive emails for enquiries</p>
-          </Box>
+          <Flex>
+            <Box>
+              <p className={styles.accountfields}>Project Updates</p>
+            </Box>
+            <Spacer />
+            <Box>
+              <Switch className={styles.switch} colorScheme="teal" size="md" />
+            </Box>
+          </Flex>
+          <p className={styles.accountsubfields}>
+            Receive emails about popular projects
+          </p>
         </Box>
-      </div>
-    );
-  }
+        <hr />
+        <Box>
+          <Flex>
+            <Box>
+              <p className={styles.accountfields}>Message Enquires</p>
+            </Box>
+            <Spacer />
+            <Box>
+              <Switch className={styles.switch} colorScheme="teal" size="md" />
+            </Box>
+          </Flex>
+          <p className={styles.accountsubfields}>
+            Receive emails for enquiries
+          </p>
+        </Box>
+      </Box>
+    </div>
+  );
+}
