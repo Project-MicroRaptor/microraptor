f26e4fbfa5180c2ae5307d6266823472d60fb02e
Tccc-web <64600163+Tccc-web@users.noreply.github.com>
2022-09-10 10:28:04 +1000

    Tycoon/MC-155 - Create campaign sections in Project View (#28)
    * Added inbox and profile icons after login
    
    * Added hovering color for inbox icon
    
    * Removed LogInOutButton.tsx, refactoring & styling
    
    * removed LogInOutButton folder
    
    * Fix navbar height
    
    * Implement profile picture dropdown
    
    * removed a duplicate line (useSession)
    
    * refactor 'View Profile' layout
    
    * refactoring scss file,add folder for its component
    
    * refactor styling, put dropdown to new component
    
    * refactor class name, code formatting
    
    * refactor styling for dropdown
    
    * refactor menuList styling
    
    * refactor dropdown style
    
    * remove !important, implement Chakra UI's styling
    
    * refactor styling
    
    * refactor styling
    
    * remove !important from css
    
    * code formatted, remove unused import
    
    * Added information for project preview (demo)
    
    * Implemented Project Preview
    
    * Implemented Project Preview
    
    * fix build error, added export default...
    
    * code format (remove spacing)
    
    * move swr.tsx file outside the /page folder
    
    * refactor codes/css, added primary font
    
    * fix screen responsive, add default value
    
    * fix indentation
    
    * fix spacing
    
    * fix styling
    
    * fix icon scaling
    
    * add spinner, 404 page error, variable "backers"
    
    * add NavBar to [id].tsx file
    
    * added spinner on error page
    
    * add spinner to !data
    
    * clickable ProjectCard, fix icon resizing
    
    * missing key is resolved for ProjectCardBrowser
    
    * change media screen px, add in title to the tab
    
    * add id to ProjectCard
    
    * add Header/title
    
    * added space to categories.tsx, fix navbar styling
    
    * fix styling, added campaign section
    
    * fix Missing "key" prop
    
    * fix missing "key" prop
    
    * fix missing "key" prop
    
    * remove spacing & refactor styling
    
    * remove spacing & refactor styling
    
    * remove any[] from reward, change section label
    
    * fix missing "key" props
    
    * set max-width for categories
    
    * fix missing "key" props
    
    * fix missing key "prop"
    
    * fix rewards prop
    
    * fix styling, remove unused div
    
    * fix text-wrap, apply hover state for clickable
    
    * remove default cursor from description
    
    * remove top reward
    
    * refactor long image size
    
    * Change header with Heading component, fix styling
    
    * remove extra space on code

--- components/ProjectInfo/ViewProject.tsx
+++ components/ProjectInfo/ViewProject.tsx
@@ -1,10 +1,13 @@
-import styles from './ViewProject.module.scss';
-import { Center, Progress, Grid, GridItem } from '@chakra-ui/react';
+import { Center, Heading, Progress } from '@chakra-ui/react';
 import { AiOutlineTag } from "react-icons/ai";
 import { HiLocationMarker } from "react-icons/hi";
 import { Button } from "@chakra-ui/react";
+
 import { ProjectCategories } from "../../types/categories";
 import type { ProjectCategory } from "../../types/categories";
+
+import styles from './ViewProject.module.scss';
+import { ProjectRewards } from '../../types/project';
 
 export interface ProjectInfo {
   id?: string;
@@ -20,7 +23,7 @@ export interface ProjectInfo {
   aboutBusiness?: string;
   aboutOwner?: string;
   businessPlan?: string;
-  rewards?: string[];
+  rewards?: Array<ProjectRewards>;
 }
 
 export default function ViewProject(props: ProjectInfo) {
@@ -63,8 +66,8 @@ export default function ViewProject(props: ProjectInfo) {
         <Center>{name}</Center>
       </span>
 
-      <Grid>
-        <GridItem className={styles.gridLeft}>
+      <div className={styles.productWrapper}>
+        <div className={styles.gridLeft}>
           <div className={styles.projectImage}>
             <img src={props?.images?.length ? props?.images[0] : "/default.png"} alt={props.name} />
           </div>
@@ -74,39 +77,92 @@ export default function ViewProject(props: ProjectInfo) {
             <HiLocationMarker className={styles.locationIcon} />
             <span className={styles.content}>{postcode}</span>
           </div>
-        </GridItem>
+        </div>
 
-        <GridItem className={styles.gridRight}>
-          <div className={styles.progressContainer}>
-            <Progress
-              value={(currentFunding / targetFunding) * 100}
-              size="sm"
-              borderStartRadius={30}
-              borderEndRadius={30}
-              className={styles.progressBar}
-            />
-            <span className={styles.fundingText}>
-              <p>
-                <span className={styles.current}> ${currentFunding?.toLocaleString()}</span> pledged of ${targetFunding?.toLocaleString()} goal
-              </p>
-            </span>
-            <span className={styles.backersAmount}>
-              <p>{backers} backers</p>
-            </span>
-            <span className={styles.daysAmount}>
-              <p>{daysRemaining()} days to go</p>
-            </span>
+        <div className={styles.progressContainer}>
+          <Progress
+            value={(currentFunding / targetFunding) * 100}
+            size="sm"
+            borderStartRadius={30}
+            borderEndRadius={30}
+            className={styles.progressBar}
+          />
+          <span className={styles.fundingText}>
+            <p>
+              <span className={styles.current}> ${currentFunding?.toLocaleString()}</span> pledged of ${targetFunding?.toLocaleString()} goal
+            </p>
+          </span>
+          <span className={styles.backersAmount}>
+            <p>{backers} backers</p>
+          </span>
+          <span className={styles.daysAmount}>
+            <p>{daysRemaining()} days to go</p>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles.buttons}>
+        <Button width="250px" borderRadius={4} fontSize={16} disabled>Share</Button>
+        <Button width="250px" borderRadius={4} fontSize={16} disabled>Fund this Project</Button>
+        <Button width="250px" borderRadius={4} fontSize={16} disabled>Enquire about Project</Button>
+      </div>
+
+      <div className={styles.campaignWrapper}>
+        <div className={styles.leftNav}>
+          <div className={styles.left}>
+            {props.aboutBusiness && <a href="#aboutBusiness">About the Business</a>}
+            {props.aboutOwner && <a href="#aboutOwner">About the Owner</a>}
+            {props.businessPlan && <a href="#businessPlan">Business Plan</a>}
+            {props.rewards && props.rewards.length > 0 && <a href="#rewards">Rewards</a>}
           </div>
-        </GridItem>
-      </Grid >
+        </div>
 
-      <Grid>
-        <GridItem className={styles.buttons}>
-          <Button width="250px" borderRadius={4} fontSize={16} disabled>Share</Button>
-          <Button width="250px" borderRadius={4} fontSize={16} disabled>Fund this Project</Button>
-          <Button width="250px" borderRadius={4} fontSize={16} disabled>Enquire about Project</Button>
-        </GridItem>
-      </Grid>
+        <div className={styles.rightNav}>
+          <div className={styles.right}>
+            {props.aboutBusiness && (
+              <>
+                <Heading id="aboutBusiness" size="md">About the Business</Heading>
+                <span>{props.aboutBusiness}</span>
+              </>
+            )}
+
+            {props.aboutOwner && (
+              <>
+                <Heading id="aboutOwner" size="md">About the Owner</Heading>
+                <span>{props.aboutOwner}</span>
+              </>
+            )}
+
+            {props.businessPlan && (
+              <>
+                <Heading id="businessPlan" size="md">Business Plan</Heading>
+                <span>{props.businessPlan}</span>
+              </>
+            )}
+
+            {props.rewards && props.rewards.length > 0 && (
+              <div className={styles.rewardButton}>
+                <Heading id="rewards" size="md">Rewards</Heading>
+                {props.rewards.map((reward: ProjectRewards, i) => {
+                  return (
+                    <span key={i}>
+                      <p className={styles.tier}><b>Reward Tier {i + 1}</b> - {reward.name}
+                        <div>
+                          Contribute ${reward.cost} or more and receive the following:
+                        </div>
+                        <div>
+                          {reward.description}
+                        </div>
+                      </p>
+                    </span>
+                  )
+                }
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div >
   )
 };
