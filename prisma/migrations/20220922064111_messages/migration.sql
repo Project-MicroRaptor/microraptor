f208400ef57dd11e4537ae06c31f1323a781f22f
Isaac <carr.isaac09@gmail.com>
2022-09-22 16:41:27 +1000

    migration

--- prisma/migrations/20220922064111_messages/migration.sql
+++ prisma/migrations/20220922064111_messages/migration.sql
@@ -1,6 +1,3 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "messageGroupId" TEXT;
-
 -- CreateTable
 CREATE TABLE "MessageGroup" (
     "id" TEXT NOT NULL,
@@ -19,11 +16,26 @@ CREATE TABLE "Message" (
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
 
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_messageGroupId_fkey" FOREIGN KEY ("messageGroupId") REFERENCES "MessageGroup"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE "_MessageGroupToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_MessageGroupToUser_AB_unique" ON "_MessageGroupToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_MessageGroupToUser_B_index" ON "_MessageGroupToUser"("B");
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "MessageGroup"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_fromId_fkey" FOREIGN KEY ("fromId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MessageGroupToUser" ADD CONSTRAINT "_MessageGroupToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "MessageGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_MessageGroupToUser" ADD CONSTRAINT "_MessageGroupToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
