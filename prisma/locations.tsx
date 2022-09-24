3d4f8056051f619cf6e6dafc7513b85cc62e3807
101511820S <101511820@student.swin.edu.au>
2022-09-24 11:07:42 +1000

    PR fixes - readme updated, file checking added.

--- prisma/locations.tsx
+++ prisma/locations.tsx
@@ -14,8 +14,10 @@ type Locations = {
 var locationPlaces: Locations[];
 
 (() => {
+  //File path. If file moved or name changed this will need to be updated.
   const csvFilePath = path.resolve(__dirname, "files/australian_postcodes.csv");
 
+  //create the column headers
   const headers = [
     "id",
     "postcode",
@@ -25,31 +27,38 @@ var locationPlaces: Locations[];
     "latitude",
   ];
 
-  const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
+  //Check if file exists and can be eccessed.
+  if (fs.existsSync(csvFilePath)) {
+    console.log("exists", csvFilePath);
+    const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
 
-  parse(
-    fileContent,
-    {
-      delimiter: ",",
-      columns: headers,
-      cast: (value, context) => {
-        if (context.header) return value;
-        if (context.column === "id") return Number(value);
-        if (context.column === "postcode") return Number(value);
-        if (context.column === "locality") return String(value);
-        if (context.column === "state") return String(value);
-        if (context.column === "longitude") return Number(value);
-        if (context.column === "latitude") return Number(value);
+    //parse the csv file and format the columns/data to the appropriate data type. If they are not cast all columns are read in as string by default.
+    parse(
+      fileContent,
+      {
+        delimiter: ",",
+        columns: headers,
+        cast: (value, context) => {
+          if (context.header) return value;
+          if (context.column === "id") return Number(value);
+          if (context.column === "postcode") return Number(value);
+          if (context.column === "locality") return String(value);
+          if (context.column === "state") return String(value);
+          if (context.column === "longitude") return Number(value);
+          if (context.column === "latitude") return Number(value);
+        },
       },
-    },
-    (error, result: Locations[]) => {
-      if (error) {
-        console.error(error);
+      (error, result: Locations[]) => {
+        if (error) {
+          console.error(error);
+        }
+        locationPlaces = result;
       }
-
-      locationPlaces = result;
-    }
-  );
+    );
+  } else {
+    console.log("File does not exist: ", csvFilePath);
+  }
 })();
 
+//export the location data read in from the csv file so the seed file can read it.
 export { locationPlaces };
