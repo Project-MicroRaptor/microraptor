a275caf1d6b79e1de50da4e05236af9fa8cd3f7c
BrandonKoala <BrandonKoala@users.noreply.github.com>
2022-10-10 15:44:39 +1100

    Fix testing

--- test/components/ProjectCreation/FormSections/MyProjectForm.test.tsx
+++ test/components/ProjectCreation/FormSections/MyProjectForm.test.tsx
@@ -4,7 +4,9 @@ import MyProjectForm from "../../../../src/components/ProjectCreation/FormSectio
 describe("MyProjectForm", () => {
   test("MyProjectForm should render correctly", () => {
     const formData = {};
-    render(<MyProjectForm formData={formData} onFormChange={() => {}} />);
+    render(
+      <MyProjectForm formData={formData} onFormChange={() => {}} errors={{}} />
+    );
   });
 
   test("MyProjectForm has data pre-populated", () => {
@@ -13,17 +15,24 @@ describe("MyProjectForm", () => {
       shortDescription: "shortDescriptionData",
       targetFunding: 100000,
       completedAt: new Date(2022, 1, 1),
-      postcode: "3000",
+      location: {
+        id: 1,
+        locality: "Melbourne",
+        state: "VIC",
+        postcode: 3000,
+        longitude: 0,
+        latitude: 0
+      }
     };
 
     const projectForm = render(
-      <MyProjectForm formData={formData} onFormChange={() => {}} />
+      <MyProjectForm formData={formData} onFormChange={() => {}} errors={{}} />
     );
 
     expect(projectForm.getByDisplayValue("nameData")).toBeTruthy();
     expect(projectForm.getByDisplayValue("shortDescriptionData")).toBeTruthy();
     expect(projectForm.getByDisplayValue("100,000")).toBeTruthy();
     expect(projectForm.getByDisplayValue("01/02/2022")).toBeTruthy();
-    expect(projectForm.getByDisplayValue("3000")).toBeTruthy();
+    expect(projectForm.getByDisplayValue("Melbourne, 3000")).toBeTruthy();
   });
 });
